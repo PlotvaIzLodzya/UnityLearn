d2f220237de45e928a1e66d3c722795cf196dcc3
PlotvaIzLodzya2 <gordoy92@gmail.com>
2021-08-28 22:37:25 +0300

    GamePlay build

---
+++ CountingPrototypeScripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _objects;
+    [SerializeField] private GameObject _spawnArea;
+    [SerializeField] private int _gravityModifier;
+    private int objectsAmount = 10;
+    public float _forceMultiplier;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ThrowingObjects();
+        Physics.gravity *= _gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void ThrowingObjects()
+    {
+        for(int i = 0; i < objectsAmount; i++)
+        {
+            int index = Random.Range(0, _objects.Length);
+            Instantiate(_objects[index], RandomSpawnPosInArea(), _objects[index].transform.rotation);
+        }
+    }
+    private Vector3 RandomSpawnPosInArea()
+    { 
+        float minPosX = _spawnArea.transform.position.x - _spawnArea.GetComponent<BoxCollider>().size.x / 2;
+        float maxPosX = _spawnArea.transform.position.x + _spawnArea.GetComponent<BoxCollider>().size.x / 2;
+        float minPosZ = _spawnArea.transform.position.z - _spawnArea.GetComponent<BoxCollider>().size.z / 2;
+        float maxPosZ = _spawnArea.transform.position.z + _spawnArea.GetComponent<BoxCollider>().size.z / 2;
+        float posX = Random.Range(minPosX, maxPosX);
+        float posZ = Random.Range(minPosZ, maxPosZ);
+        Vector3 objectPos = new Vector3(posX, _spawnArea.transform.position.y, posZ);
+        return objectPos;
+    }
+}
---
+++ CountingPrototypeScripts/Objects.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objects : MonoBehaviour
+{
+
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    private Rigidbody objectRigidbody;
+    private Vector3 startPoint;
+    private Vector3 releasePoint;
+    private Vector3 throwDirecation;
+    private bool isDragged;
+    private float _forceMultiplier;
+    private GameManager gameManagerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectRigidbody = GetComponent<Rigidbody>();
+        gameManagerScript = GameObject.Find("GameManager").GetComponent<GameManager>();
+        this._forceMultiplier = gameManagerScript._forceMultiplier;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        ConstrainObject();
+    }
+
+
+
+    void OnMouseDown()
+    {
+        
+        isDragged = true;
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        gameObject.transform.position = new Vector3(0, transform.position.y, transform.position.z);
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+
+        StartCoroutine("SettingStartPoint");
+    }
+
+    void OnMouseDrag()
+    {
+        if (isDragged)
+        {
+            Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+            transform.position = curPosition;
+        }
+    }
+    private void OnMouseUp()
+    {
+        if (isDragged)
+        {
+            releasePoint = transform.position;
+            throwDirecation = releasePoint - startPoint;
+            isDragged = false;
+            objectRigidbody.AddForce(throwDirecation * _forceMultiplier, ForceMode.Impulse);
+        }
+    }
+
+    IEnumerator SettingStartPoint()
+    {
+        while (isDragged)
+        {
+            startPoint = transform.position;
+            yield return new WaitForSeconds(0.25f);
+        }
+    }
+
+    private void ConstrainObject()
+    {
+        objectRigidbody.useGravity = !isDragged;
+        if (isDragged)
+        {
+            objectRigidbody.velocity = Vector3.zero;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground") && isDragged)
+        {
+            isDragged = false; 
+        }
+    }
+
+
+}
