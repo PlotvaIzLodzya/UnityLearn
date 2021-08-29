68374cda44bf48169ea3b7b9d26fdfe0c5e46759
PlotvaIzLodzya2 <gordoy92@gmail.com>
2021-08-29 13:17:28 +0300

    Better controlls, some organization in repository

---
+++ CountingPrototypeScripts/Version 2.0/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _objects;
+    [SerializeField] private GameObject _spawnArea;
+    [SerializeField] private int _gravityModifier;
+    private int objectsAmount = 10;
+    public float _forceMultiplier;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ThrowingObjects();
+        Physics.gravity *= _gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void ThrowingObjects()
+    {
+        for(int i = 0; i < objectsAmount; i++)
+        {
+            int index = Random.Range(0, _objects.Length);
+            Instantiate(_objects[index], RandomSpawnPosInArea(), _objects[index].transform.rotation);
+        }
+    }
+    private Vector3 RandomSpawnPosInArea()
+    {
+        Vector3 center = _spawnArea.transform.position;
+        float halfSizeX =  _spawnArea.GetComponent<BoxCollider>().size.x *_spawnArea.transform.localScale.x / 2;
+        float halfSizeZ = _spawnArea.GetComponent<BoxCollider>().size.z * _spawnArea.transform.localScale.z / 2;
+        float posX = Random.Range(-halfSizeX, halfSizeX);
+        float posZ = Random.Range(-halfSizeZ, halfSizeZ);
+        Vector3 objectPos = new Vector3(posX, _spawnArea.transform.position.y, posZ);
+        return objectPos + center;
+    }
+}
---
+++ CountingPrototypeScripts/Version 2.0/Objects.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objects : MonoBehaviour
+{
+
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    private Rigidbody objectRigidbody;
+    private Vector3 startPoint;
+    private Vector3 releasePoint;
+    private Vector3 throwDirecation;
+    private bool isDragged;
+    private GameManager gameManagerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectRigidbody = GetComponent<Rigidbody>();
+        gameManagerScript = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        ConstrainObject();
+    }
+
+
+
+    void OnMouseDown()
+    {
+        isDragged = true;
+    }
+
+    void OnMouseDrag()
+    {
+        isDragged = true;
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        gameObject.transform.position = new Vector3(0, transform.position.y, transform.position.z);
+        Vector3 forceDirection = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z)) - transform.position;
+        objectRigidbody.AddForce(forceDirection*gameManagerScript._forceMultiplier, ForceMode.VelocityChange);
+
+    }
+    private void OnMouseUp()
+    {
+        isDragged = false;
+    }
+
+
+    private void ConstrainObject()
+    {
+        objectRigidbody.useGravity = !isDragged;
+        if (isDragged)
+        {
+            objectRigidbody.velocity = Vector3.zero;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground") && isDragged)
+        {
+            isDragged = false; 
+        }
+    }
+
+
+}
