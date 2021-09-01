25a8bfabe3c0fb28c8981a4334c966aee347f4f5
PlotvaIzLodzya <86444967+PlotvaIzLodzya@users.noreply.github.com>
2021-09-02 00:41:00 +0300

    Added some scene flow persistence

---
+++ JrProject/Assets/Scripts/MainManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    public static MainManager Instance;
+    public Color TeamColor;
+    private void Awake()
+    {
+        if(Instance!=null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    public void NewColorSerlected(Color color)
+    {
+        MainManager.Instance.TeamColor = color;
+    }
+}
+
