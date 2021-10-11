cc6cb0637b8bb46a5d7bd23c3c60354bc76fcaf9
PlotvaIzLodzya <86444967+PlotvaIzLodzya@users.noreply.github.com>
2021-10-11 14:19:36 +0300

    Update Spawner.cs

--- iJunior/Spawns/Spawners/Assets/Scripts/Spawner.cs
+++ iJunior/Spawns/Spawners/Assets/Scripts/Spawner.cs
@@ -4,24 +4,26 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private List<Vector3> _spawners;
+    [SerializeField] private List<Vector3> _spawnPoint;
     [SerializeField] private List<GameObject> _enemys;
-    [SerializeField] private WaitForSeconds _spawnRate = new WaitForSeconds(2);
+    [SerializeField] private WaitForSeconds _spawnRate;
+    [SerializeField] private float _spawnCooldown = 2;
     [SerializeField] private bool _isGameActive = true;
 
     private void Start()
     {
-        Debug.Log(_spawners.Count + " " + _enemys.Count);
         StartCoroutine(SpawnEnemys());
     }
 
     private IEnumerator SpawnEnemys()
     {
+        _spawnRate = new WaitForSeconds(_spawnCooldown);
+
         while (_isGameActive) 
         {
             int enemyIndex = Random.Range(0, _enemys.Count);
-            int spawnerIndex = Random.Range(0, _spawners.Count);
-            Instantiate(_enemys[enemyIndex], _spawners[spawnerIndex], Quaternion.identity);
+            int spawnerIndex = Random.Range(0, _spawnPoint.Count);
+            Instantiate(_enemys[enemyIndex], _spawnPoint[spawnerIndex], Quaternion.identity);
             yield return _spawnRate;
         }
     }
